fdc2097009212e85b8f1a40d1c39297923f52341
ella <jellyella321@gmail.com>
2026-08-24 23:08:28 -0700

    added robot container

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,72 @@
+// Copyright (c) 2025 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot;
+
+import choreo.auto.AutoChooser;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.sim.SimMechs;
+import frc.robot.utils.AutoConfig;
+import frc.robot.utils.MappedXboxController;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RobotContainer {
+
+  public final MappedXboxController m_driverController =
+      new MappedXboxController(ControllerConstants.kDriverControllerPort, "Driver");
+  public final MappedXboxController m_operatorController =
+      new MappedXboxController(ControllerConstants.kOperatorControllerPort, "Operator");
+
+  /// sim file for intakepivot needs to be added -- seems like its not been merged yet
+
+  private AutoChooser autoChooser = new AutoChooser();
+
+  private List<AutoConfig> autos = new ArrayList<>();
+
+  private SendableChooser<AutoConfig> autoVisualizer = new SendableChooser<AutoConfig>();
+  private Field2d field2d = new Field2d();
+
+  public RobotContainer() {
+
+    configureSwerve();
+    configureChoreoAutoChooser();
+    configureOperatorBinds();
+    configureAutoVisualizer();
+    if (Utils.isSimulation()) {
+      SimMechs.getInstance().publishToNT();
+    }
+  }
+
+  private void configureOperatorBinds() {}
+
+  private void configureChoreoAutoChooser() {}
+
+  private void configureAutoVisualizer() {
+
+    for (AutoConfig auto : autos) {
+      autoVisualizer.addOption(auto.name, auto);
+    }
+
+    autoVisualizer.onChange(
+        (trajNames) -> {
+          autoChooser.select(trajNames.name);
+        });
+
+    SmartDashboard.putData("Auto Visualizer", autoVisualizer);
+    SmartDashboard.putData("Field Visualize", field2d);
+  }
+
+  private void configureSwerve() {
+    SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric();
+  }
+}
