aa49ff076dcfb10da6fddb9608a155c58c33b948
isabella354 <isabellayu817@gmail.com>
2026-08-26 00:12:50 -0700

    spotless simmechs

--- src/main/java/frc/robot/sim/SimMechs.java
+++ src/main/java/frc/robot/sim/SimMechs.java
@@ -7,24 +7,17 @@
 
 package frc.robot.sim;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.Constants;
 
 public final class SimMechs {
 
   public final Mechanism2d mech =
       new Mechanism2d(Constants.SimulationConstants.kDrivebaseWidth.in(Meters), 1.0);
-
 
   private static SimMechs instance = null;
 
