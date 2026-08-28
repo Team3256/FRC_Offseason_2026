70aa6da940fa39e72ab7e00f2d3615541b946de0
jolina <jolinatluo@gmail.com>
2026-08-27 17:18:35 -0700

    fixed sim!

--- src/main/java/frc/robot/sim/SimMechs.java
+++ src/main/java/frc/robot/sim/SimMechs.java
@@ -7,11 +7,17 @@
 
 package frc.robot.sim;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.Constants;
 
 public final class SimMechs {
@@ -23,7 +29,7 @@ public final class SimMechs {
       mech.getRoot(
           "Shooter Pivot",
           Constants.SimulationConstants.kDrivebaseWidth.in(Meters) / 2 - 0.05,
-          Inches.of(14).in(Meters));
+          Inches.of(25).in(Meters));
 
   private final MechanismLigament2d shooterPivotViz =
       shooterPivotRoot.append(
