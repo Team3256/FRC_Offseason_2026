43041be0516b8778578346b6e4a2abc6a339944a
Ashvath <ashvath.ramesh@warriorlife.net>
2026-08-28 09:02:07 -0700

    Merge main into intake-rollers

--- src/main/java/frc/robot/subsystems/intakerollers/IntakeRollers.java
+++ src/main/java/frc/robot/subsystems/intakerollers/IntakeRollers.java
@@ -1,10 +1,3 @@
-// Copyright (c) 2025 FRC 3256
-// https://github.com/Team3256
-//
-// Use of this source code is governed by a
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
 package frc.robot.subsystems.intakerollers;
 
 import edu.wpi.first.wpilibj2.command.Command;
